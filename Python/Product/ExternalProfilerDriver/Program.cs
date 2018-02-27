5e6ea99782fe2985a33b6b7d67844c566d6907b0
R. G. Esteves <rgesteves@gmail.com>
2018-02-27 12:48:33 -0800

    Skeleton for external profiler driver

---
+++ Python/Product/ExternalProfilerDriver/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Microsoft.PythonTools.Profiling.ExternalProfilerDriver {
+    class Program {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing an executable");
+        }
+    }
+}
